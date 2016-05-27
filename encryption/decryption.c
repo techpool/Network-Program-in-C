9a5c7e7523251102603d8302c01183d2f5ac0e88
Suryadeep Pal <suryadeep10@gmail.com>
2016-05-27 17:14:45 +0530

    encryption and decryption program with c added

---
+++ encryption/decryption.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+// GLOBAL VARIABLES
+// The souorce array much like the encryption program contains the sequence of
+// the english alphabets
+char source[26] = { "abcdefghijklmnopqrstuvwxyz" };
+
+// The destionation array is going to contain the same alphabets but in a jumbled
+// fashion.
+// ***** THIS IS IMPORTANT THAT THE SAME JUMBLED SEQUENCE OF CHARACTERS SHOULD BE 
+// USED IN BOTH THE ENCRYPTION AND DECRYPITON PROCESS ELSE YOU WONT BE GETTING THE
+// PROPER OUTPUT *****
+char destination[26] = { "efdvtmlxnzqgurakjwbcyhoips" };
+
+int main(){
+
+	// FILE pointers for the same purpose which will help handling of the two file
+	// which will be of use
+	FILE *fs, *fd;
+
+	int val, i;
+	char ch;
+
+	// fs and fd are the file handlers for the two files
+	// the 'cipher.txt' file is going to contain the file with cipher text
+	// and the 'result.txt' file is going to contain the file with the decyphered
+	// text
+	fs = fopen("cipher.txt", "r");
+	fd = fopen("result.txt", "w");
+
+	// This loop will continue until we get a EOF character in the file
+	while(1){
+		
+		// This function retrieves one character from the file at a time
+		ch = fgetc(fs);
+
+		// This is a flag variable which will help us to check later whether the
+		// retrived character is there in our destination arrray or not
+		val = -1;
+		
+		// If we encounter a EOF flag which indicated that it has reached the End Of File
+		// Then it will exit the loop
+		if (ch == EOF)
+		{
+			break;
+		}
+
+		// This for loop scans through the destination array for a match from
+		// the character that was retrieved from the fgetc function
+		for (i = 0; i < 25; ++i)
+		{
+			// If a match is found then the position of the character is stored in
+			// the val variable
+			if (ch == destination[i])
+			{
+				val = i;
+				break;
+			}
+		}
+
+		// Now here we check whether the val variable is having a value or -1
+		// If it is -1 it means no match was foound for the retieved character in the
+		// destination array and so we store the same character in the file as well
+		if (val == -1)
+		{
+			fprintf(fd, "%c", ch);
+		}
+
+		// In case we have a position for that character in the destination array
+		// then we match the same location with the source array and store the result
+		// in the destination file.
+		else
+		{
+			fprintf(fd, "%c", source[val]);
+		}
+	}
+}
---
+++ encryption/encryption.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+// GLOBAL VARIABLES
+// The source is the original sequence of characters in the english 
+// alphabet series
+
+char source[26] = { "abcdefghijklmnopqrstuvwxyz" };
+
+// The destination is the the mapping which will be done for the encryption
+// procedure in order to form the cipher text
+// For example:
+// 'a' --> 'e'
+// 'b' --> 'f'
+
+// and so on in the cipher text
+char destination[26] = { "efdvtmlxnzqgurakjwbcyhoips" };
+
+int main()
+{
+
+	// Two file pointers which will be used to handle two files
+	FILE *fs, *fd;
+	int i, val;
+	char ch;
+
+	// Openning of two files, one the plain text file
+	// which will contain the messages and the other is the
+	// cipher text which will contain the cipher text
+
+	// Opened in read mode since we only need to read plain text file
+	fs = fopen("plain.txt", "r");
+
+	// Opened in write mode since we only need to write cipher text file
+	fd = fopen("cipher.txt", "w"); 
+
+	// Read the file unless we encounter a EOF flag
+	while(1){
+
+		// Retrieving the character from the source or plaint text file
+		ch = fgetc(fs);
+
+		// Setting a flag variable which will help us to check whether the
+		// character recieved from the file is there in our source alphabet array
+		val = -1;
+
+		// If the character retrieved is a EOF flag then we have reached the end
+		// of the file and we exit the loop
+		if (ch == EOF)
+		{
+			break;
+		}
+
+		// We take the character and we scan for the character in our array for a match
+		for (i = 0; i < 25; ++i)
+		{
+
+			// In case we find a match we we break out of the loop
+			// and store the position of the character in the variable val
+			if (ch == source[i])
+			{
+				val = i;
+				break;
+			}
+		}
+
+		// If the val is -1 or we did not get a matching character in the array
+		// then we print the file with the same character in the source file
+		if (val == -1)
+		{
+			fprintf(fd, "%c", ch);
+		}
+
+		// else we match the character in the source character and destination
+		// character and we print the value in the destination file
+		else
+		{
+			fprintf(fd, "%c", destination[val]);
+		}
+	}
+}
